1bd96d0a03dad50889fbd99bb4526fa5c5d4a403
Alyssa Smith <smith.alyss@northeastern.edu>
2026-02-10 22:03:09 -0500

    added changelog; working parser script

--- parse_reposts_and_extract_follow_timings.py
+++ parse_reposts_and_extract_follow_timings.py
@@ -3,13 +3,15 @@ import gc
 import json
 from zoneinfo import ZoneInfo
 
+import numpy as np
 import polars as pl
 from polars.datatypes import String, Int64, Datetime
 
-AB_DIDS = json.load(open('handles_to_dids.json', 'r'))
+FILEPATH = '/scratch/nte5cp'
+AB_DIDS = json.load(open(f'{FILEPATH}/handles_to_dids.json', 'r'))
 
-# FILEPATH = '/scratch/nte5cp'
-FILEPATH = '/Users/a404/attention-brokers-bsky/'
+
+# FILEPATH = '/Users/a404/attention-brokers-bsky/'
 HANDLE = 'jortsthecat.bsky.social'
 AB_DID = AB_DIDS[HANDLE]
 REPOST_CUTOFF = dt.datetime(year=2025, month=9, day=15, tzinfo=ZoneInfo("UTC"))
@@ -29,7 +31,26 @@ def parse_repost_dict(repost_dict):
         'created_at': created_at.replace('Z', '+00:00'),
     }
 
-# df_follows = pl.read_csv(f'{FILEPATH}/follows.csv', header=False, new_columns=["from", "to", "created_at"])
+df_follows = pl.read_csv(
+    f'{FILEPATH}/follows_all.csv', 
+    has_header=False, 
+    new_columns=["from", "to", "created_at"],
+)
+df_follows = df_follows.drop_nulls()
+df_follows = df_follows.with_columns(
+    pl.when(
+        pl.col('created_at').str.tail(1) == 'Z').then(
+        pl.col('created_at').str.head(-1).str.to_datetime(
+            format='%Y-%m-%dT%H:%M:%S%.3f', 
+            time_zone='UTC',
+            strict=False,
+        )
+    ).otherwise(pl.col('created_at').str.to_datetime(
+        format="%Y-%m-%dT%H:%M:%S%.6f", 
+        time_zone="UTC",
+        strict=False,
+    ))
+)
 
 followers_of_ab = df_follows.filter(pl.col('to') == AB_DID)
 
@@ -44,15 +65,16 @@ df_reposts = df_reposts.with_columns(
 )
 df_reposts = df_reposts.filter(pl.col('created_at') <= REPOST_CUTOFF)
 df_reposts = df_reposts.filter(pl.col('orig_poster') != AB_DID)
-df_reposts = df_reposts.group_by(pl.col('reposted')).agg(pl.col('created_at').min())
+df_reposts = df_reposts.group_by(pl.col('orig_poster')).agg(pl.col('created_at').min())
 
-MIN_REPOST_DAY = df_reposts.select(pl.col('created_at')).min() 
+MIN_REPOST_DAY = df_reposts.select(pl.col('created_at')).min().item()
+print(MIN_REPOST_DAY)
 
 data_final = []
-for ix, row in enumerate(df_reposts.iter_rows(names=True)):
-    repost_created_at = row['created_at']
-    repost_period = (repost_created_at - MIN_REPOST_DAY).days
-
+for ix, row in enumerate(df_reposts.iter_rows(named=True)):
+    repost_created_at = pl.DataFrame({'created_at': [row['created_at']]})
+    repost_period = (repost_created_at.item() - MIN_REPOST_DAY).days
+    
     orig_poster = row['orig_poster']
     low_follow_bound = row['created_at'] - dt.timedelta(days=14)
     high_follow_bound = row['created_at'] + dt.timedelta(days=14)
@@ -61,6 +83,9 @@ for ix, row in enumerate(df_reposts.iter_rows(names=True)):
         (pl.col('created_at') <= high_follow_bound) & \
         (pl.col('created_at') >= low_follow_bound) & \
         (pl.col('to') == orig_poster)
+    )
+    follows_to_op = follows_to_op.with_columns(
+        pl.lit(repost_created_at.item(), dtype=Datetime).alias('repost_created_at')
     )
     follows_to_op_following_ab = follows_to_op.join(
         followers_of_ab, 
@@ -73,9 +98,9 @@ for ix, row in enumerate(df_reposts.iter_rows(names=True)):
 
     # first, figure out when the follower --> reposted tie happened relative to the repost
     follows_to_op_following_ab = follows_to_op_following_ab.with_columns(
-        ((repost_created_at - pl.col('created_at')).days).alias('days_before_after_repost')
+        ((pl.col('repost_created_at').sub(pl.col('created_at'))).dt.total_days()).alias('days_before_after_repost'),
+        (pl.col('created_at_from_ab').fill_null(repost_created_at.item()))
     )
-
     followers_before_repost = follows_to_op_following_ab.filter(
         pl.col('days_before_after_repost') < 0
     )
@@ -83,11 +108,11 @@ for ix, row in enumerate(df_reposts.iter_rows(names=True)):
         pl.col('days_before_after_repost') >= 0
     )
 
-    followers_before_repost.with_column(
-        ((pl.col('created_at') - pl.col('created_at_from_ab')).total_seconds() > 0).alias('ab_follower')
+    followers_before_repost = followers_before_repost.with_columns(
+        ((pl.col('created_at').sub(pl.col('created_at_from_ab'))).dt.total_seconds() > 0).alias('ab_follower')
     )
-    followers_after_repost.with_column(
-        ((repost_created_at - pl.col('created_at_from_ab')).total_seconds() > 0).alias('ab_follower')
+    followers_after_repost = followers_after_repost.with_columns(
+        ((pl.col('repost_created_at').sub(pl.col('created_at_from_ab'))).dt.total_seconds() > 0).alias('ab_follower')
     )
 
     followers_before_repost = followers_before_repost.group_by(
@@ -95,35 +120,23 @@ for ix, row in enumerate(df_reposts.iter_rows(names=True)):
     followers_after_repost = followers_after_repost.group_by(
         [pl.col('days_before_after_repost'), pl.col('ab_follower')]).agg(pl.col('from').count())
     
-    for row in followers_before_repost.iter_rows():
-        if row['ab_follower']:
-            ts = row['days_before_after_repost']
-        else:
-            ts = np.inf
+    for row in followers_before_repost.iter_rows(named=True):
         data_final.append({
             'gain_rate': row['from'],
-            'treated': 0,
+            'ever_treated': row['ab_follower'],
             'unit_id': ix,
             'time_period': repost_period + row['days_before_after_repost'],
-            'ts': ts,
+            'ts': row['days_before_after_repost'],
         })
 
-    for row in followers_after_repost.iter_rows():
-        if row['ab_follower']:
-            ts = row['days_before_after_repost']
-        else:
-            ts = np.inf
+    for row in followers_after_repost.iter_rows(named=True):
         data_final.append({
             'gain_rate': row['from'],
-            'treated': row['ab_follower'],
+            'ever_treated': row['ab_follower'],
             'unit_id': ix,
             'time_period': repost_period + row['days_before_after_repost'],
-            'ts': ts,
+            'ts': row['days_before_after_repost'],
         })
 
 data = pl.DataFrame(data_final)    
-data.write_csv(f'did_csvs/{HANDLE}.csv')
-
-
-    
-    
+data.write_csv(f'{FILEPATH}/did_csvs/{HANDLE}.csv')
